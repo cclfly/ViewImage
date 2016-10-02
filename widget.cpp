e4e52c73ac78381835af6a61c37f37bfba82c782
cclfly <1171407627@qq.com>
2016-10-03 00:29:43 +0800

    拖放窗口大小，图片大小随窗口大小变化，并且显示居中

--- widget.cpp
+++ widget.cpp
@@ -5,22 +5,54 @@
 #include <QMovie>
 
 Widget::Widget(QWidget *parent)
-    : QWidget(parent), labImg(new QLabel(this)), mv(NULL)
+    : QWidget(parent), mv(NULL), imgMagn(0.5)
 {
-    QString imgPath = QApplication::arguments()[1];
-    QImage img(imgPath);
-    imgExt = imgPath.right(imgPath.length()-imgPath.lastIndexOf('.'));
-    imgSize = img.size();
-    labImg->setPixmap(QPixmap::fromImage(img));
-    labImg->setScaledContents(true);
-    labImg->resize(imgSize*0.5);
+    labImg = new QLabel(this);
 
+    QString imgPath = QApplication::arguments()[1];
+    img = new QImage(imgPath);
+    imgExt = imgPath.right(imgPath.length()-imgPath.lastIndexOf('.'));
+    imgConstSize = img->size();
+    imgSize = imgConstSize*0.5;
+    labImg->setPixmap(QPixmap::fromImage(*img));
+    labImg->setScaledContents(true);
+    labImg->resize(imgSize);
+
+    //这里应该判断，，但是算了
+    //if(imgExt==".gif"){
     mv = new QMovie(imgPath);
     mv->start();
     labImg->setMovie(mv);
+    //}
 }
 
 Widget::~Widget()
 {
+    if(mv)
+    {
+        mv->stop();
+        delete mv;
+    }
     delete labImg;
+}
+#include <QDebug>
+void Widget::resizeEvent(QResizeEvent *event)
+{
+    QSize newSize = event->size();
+    double slope = imgConstSize.height()*1.0/imgConstSize.width();
+
+    if(newSize.height()*1.0/newSize.width()>slope)
+    {
+        imgMagn = newSize.width()*1.0/imgConstSize.width();
+        imgSize = imgMagn*imgConstSize;
+        imgPot = QPoint(0,(newSize.height()-imgSize.height())/2);
+    }
+    else
+    {
+        imgMagn = newSize.height()*1.0/imgConstSize.height();
+        imgSize = imgMagn*imgConstSize;
+        imgPot = QPoint((newSize.width()-imgSize.width())/2,0);
+    }
+
+    labImg->setGeometry(QRect(imgPot,imgSize));
 }
--- widget.h
+++ widget.h
@@ -3,6 +3,8 @@
 
 #include <QWidget>
 #include <QLabel>
+#include <QScrollBar>
+#include <QResizeEvent>
 
 class Widget : public QWidget
 {
@@ -12,10 +14,20 @@ public:
     Widget(QWidget *parent = 0);
     ~Widget();
 private:
+    //窗口大小改变事件
+    void resizeEvent(QResizeEvent *event);
+private:
+    QImage *img;            //图片本体
     QLabel *labImg;         //放图片的label
-    QSize imgSize;          //图片的大小
+    QSize imgConstSize;     //图片的原始大小
+    QPoint imgPot;          //图片在窗口中的位置
+    QSize imgSize;          //图片显示大小
     QString imgExt;         //图片的扩展名
-    QMovie *mv;              //图片本体包括gif
+    QMovie *mv;             //图片动画本体如gif
+
+    double imgMagn;         //图片放大倍数
+
+    // //缩略图窗口
 };
 
 #endif // WIDGET_H
